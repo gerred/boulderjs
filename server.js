78da175b2c18dbb558307e516daf1bd8fa2bb0ef
Gerred Dillon <hello@gerred.com>
2012-04-06 01:01:53 -0600

    Initial ocmmit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib'),
+    public = __dirname + "/public";
+
+var app = express.createServer();
+
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .use(nib());
+}
+
+app.configure(function() {
+    app.use(stylus.middleware({
+        src: public,
+        compile: compile
+    }));
+    app.use(express.static(public));
+    app.set('view engine', 'jade');
+    app.set('view options', {layout: false});
+    app.use(express.logger());
+})
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log("Server listening on port %d in %s mode",
+    port,
+    app.settings.env
+);
